82da76f4e086f42e7d26a1dfd00d0144192c0404
tekriwaltwinkle <twinkletekriwal1696@gmail.com>
2019-02-21 16:40:03 +0530

    hh

---
+++ quiz_backend/Migrations/20190221100554_InitialMigrationScript.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace quiz_backend.Migrations
+{
+    public partial class InitialMigrationScript : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Text = table.Column<string>(nullable: true),
+                    CorrectAnswer = table.Column<string>(nullable: true),
+                    Answer1 = table.Column<string>(nullable: true),
+                    Answer2 = table.Column<string>(nullable: true),
+                    Answer3 = table.Column<string>(nullable: true),
+                    QuizId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quiz",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quiz", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Quiz");
+        }
+    }
+}
---
+++ quiz_backend/Models/Quiz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace quiz_backend.Models
+{
+    public class Quiz
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+    }
+}
